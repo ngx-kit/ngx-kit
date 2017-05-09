93d0969668d24516a0eedbbbe044b5e9d04133b3
navix <inavix@gmail.com>
2017-05-09 10:09:11 +0300

    @ngx-kit/pagination alpha

--- packages/pagination/kit-pagination.module.ts
+++ packages/pagination/kit-pagination.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { KitPaginationComponent } from './kit-pagination/kit-pagination.component';
 import { KitPaginationService } from './kit-pagination.service';
 
 @NgModule({
-  imports: [],
+  imports: [
+    CommonModule,
+  ],
   exports: [
     KitPaginationComponent,
   ],
--- packages/pagination/kit-pagination/kit-pagination.component.ts
+++ packages/pagination/kit-pagination/kit-pagination.component.ts
@@ -5,19 +5,36 @@ import { KitCoreService } from '@ngx-kit/core';
 
 import { KitPaginationService } from '../kit-pagination.service';
 
+/**
+ * @todo use button-group for styling
+ */
 @Component({
   selector: 'kit-pagination',
   template: `
-    pagination
+    <button *ngIf="displayFirstLink"
+            (click)="pageChange.emit(1)">First
+    </button>
+    <button *ngIf="displayPrevLink && currentPage > 1"
+            (click)="pageChange.emit(currentPage - 1)">Prev
+    </button>
+    <button *ngFor="let page of pages"
+            (click)="pageChange.emit(page)">{{ page }}
+    </button>
+    <button *ngIf="displayNextLink && currentPage < totalPages"
+            (click)="pageChange.emit(currentPage + 1)">Next
+    </button>
+    <button *ngIf="displayLastLink"
+            (click)="pageChange.emit(totalPages)">Last
+    </button>
   `,
 })
 export class KitPaginationComponent implements OnInit {
 
-  @Input() total: number;
-  @Input() current: number;
-  @Input() onPage: number;
+  @Input() totalRows: number;
+  @Input() currentPage: number;
+  @Input() rowsPerPage: number;
 
-  @Input() pageLinksNumber = 4;
+  @Input() pageLinksNumber = 5;
   @Input() displayNextLink: boolean = true;
   @Input() displayPrevLink: boolean = true;
   @Input() displayFirstLink: boolean = true;
@@ -27,6 +44,9 @@ export class KitPaginationComponent implements OnInit {
 
   @HostBinding('class') hostClass: string;
 
+  pages: number[] = [];
+  totalPages: number;
+
   constructor(private core: KitCoreService,
               private service: KitPaginationService) {
   }
@@ -34,6 +54,29 @@ export class KitPaginationComponent implements OnInit {
   ngOnInit() {
     this.compileStyles();
     this.calcStyles();
+  }
+
+  ngOnChanges() {
+    this.recalcPages();
+  }
+
+  private recalcPages() {
+    // calc total pages
+    this.totalPages = Math.ceil(this.totalRows / this.rowsPerPage);
+    // calc sides
+    const half = Math.floor(this.pageLinksNumber / 2);
+    const range = this.pageLinksNumber - 1;
+    let leftSide = this.currentPage <= half ? 1 : this.currentPage - half;
+    const rightSide = this.totalPages <= leftSide + range ? this.totalPages : leftSide + range;
+    // check left range at the end
+    if (rightSide === this.totalPages && range < this.totalPages) {
+      leftSide = rightSide - range;
+    }
+    // compile range
+    this.pages = [];
+    for (let i = leftSide; i <= rightSide; i++) {
+      this.pages.push(i);
+    }
   }
 
   private compileStyles() {
