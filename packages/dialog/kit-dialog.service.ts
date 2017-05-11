541068c9cb994ee5b0474a17d4852c35713b172b
navix <inavix@gmail.com>
2017-05-11 14:44:55 +0300

    dialog: use core/host.service

--- packages/dialog/kit-dialog.service.ts
+++ packages/dialog/kit-dialog.service.ts
@@ -1,9 +1,9 @@
 import {
-  ApplicationRef, ComponentFactoryResolver, ComponentRef, EmbeddedViewRef, Injectable, Injector,
+  ComponentRef, Injectable,
   Type
 } from '@angular/core';
 
-import { KitComponentService, KitCoreService, KitThemeProps } from '@ngx-kit/core';
+import { KitComponentService, KitCoreService, KitHostService, KitThemeProps } from '@ngx-kit/core';
 
 import { KitDialogTheme, DialogHandlers } from './interfaces';
 
@@ -12,12 +12,9 @@ export class KitDialogService extends KitComponentService<KitDialogTheme> {
 
   private themeProps: KitThemeProps;
   private dialogRef: ComponentRef<any>;
-  private rootRef: ComponentRef<any>;
 
   constructor(private kitCore: KitCoreService,
-              private resolver: ComponentFactoryResolver,
-              private applicationRef: ApplicationRef,
-              private injector: Injector) {
+              private host: KitHostService) {
     super();
     // theming
     this.themeProps = this.kitCore.getThemeProps();
@@ -28,32 +25,13 @@ export class KitDialogService extends KitComponentService<KitDialogTheme> {
   private compileTheme() {
     this.theme = {
       host: {
-        base: {
-        },
+        base: {},
       }
     };
   }
 
-  /**
-   * Create element into root container.
-   *
-   * @param component
-   * @return {ComponentRef<T>}
-   */
-  private create<T>(component: Type<T>): ComponentRef<T> {
-    this.detectRoot();
-    const factory = this.resolver.resolveComponentFactory(component);
-    console.log('factory', factory);
-    const componentRef = factory.create(this.injector);
-    console.log('componentRef:hv', componentRef.hostView);
-    console.log('componentRef', componentRef);
-    this.applicationRef.attachView(componentRef.hostView);
-    (this.rootRef.hostView as EmbeddedViewRef<any>).rootNodes[0].appendChild((componentRef.hostView as EmbeddedViewRef<any>).rootNodes[0]);
-    return componentRef;
-  }
-
   show<T extends DialogHandlers>(component: Type<T>): T {
-    let modalRef: ComponentRef<T> = this.create<T>(component);
+    let modalRef: ComponentRef<T> = this.host.host<T>(component);
     let instance: T = modalRef.instance;
     // subscribe to modal events
     // close event
@@ -69,13 +47,6 @@ export class KitDialogService extends KitComponentService<KitDialogTheme> {
 
   hide() {
     this.dialogRef.destroy();
-  }
-
-  private detectRoot() {
-    if (!this.rootRef) {
-      this.rootRef = this.applicationRef['_rootComponents'][0];
-      console.log('rR', this.rootRef, this.applicationRef);
-    }
   }
 
 }
