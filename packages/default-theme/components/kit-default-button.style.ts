fb1ceb47a45b3e02985f3531927b1e1391dc2494
icydew <icydew@live.com>
2017-06-22 16:22:19 +0300

    fix(default-theme): hover for buttons

--- packages/default-theme/components/kit-default-button.style.ts
+++ packages/default-theme/components/kit-default-button.style.ts
@@ -1,14 +1,15 @@
 import { Inject, Injectable } from '@angular/core';
 
 import { KitComponentStyle, kitTheme } from '@ngx-kit/core';
-import { RegistrationDef } from '@ngx-kit/styler';
+import { RegistrationDef, StylerColorService } from '@ngx-kit/styler';
 
 import { KitDefaultThemeService } from '../kit-default-theme.service';
 
 @Injectable()
 export class KitDefaultButtonStyle implements KitComponentStyle {
 
-  constructor(@Inject(kitTheme) private theme: KitDefaultThemeService) {
+  constructor(@Inject(kitTheme) private theme: KitDefaultThemeService,
+              private color: StylerColorService) {
   }
 
   getStyles(): RegistrationDef {
@@ -60,26 +61,51 @@ export class KitDefaultButtonStyle implements KitComponentStyle {
               background: params.colors.button.color,
               color: params.colors.button.text,
               borderColor: params.colors.border.color,
+              $nest: {
+                '&:hover': {
+                  background: this.theme.colorMod(.05, params.colors.button.color),
+                },
+              }
             },
             primary: {
               background: params.colors.brand.color,
               borderColor: params.colors.brand.color,
               color: params.colors.brand.text,
+              $nest: {
+                '&:hover': {
+                  background: this.theme.colorMod(.05, params.colors.brand.color),
+                },
+              }
             },
             success: {
               background: params.colors.success.color,
               borderColor: params.colors.success.color,
               color: params.colors.success.text,
+              $nest: {
+                '&:hover': {
+                  background: this.theme.colorMod(.05, params.colors.success.color),
+                },
+              }
             },
             warning: {
               background: params.colors.warning.color,
               borderColor: params.colors.warning.color,
               color: params.colors.warning.text,
+              $nest: {
+                '&:hover': {
+                  background: this.theme.colorMod(.05, params.colors.warning.color),
+                },
+              }
             },
             error: {
               background: params.colors.error.color,
               borderColor: params.colors.error.color,
               color: params.colors.error.text,
+              $nest: {
+                '&:hover': {
+                  background: this.theme.colorMod(.05, params.colors.error.color),
+                },
+              }
             },
             link: {
               boxShadow: 'none',
@@ -88,6 +114,12 @@ export class KitDefaultButtonStyle implements KitComponentStyle {
               background: params.colors.link.color,
               borderColor: 'transparent',
               color: params.colors.link.text,
+              $nest: {
+                '&:hover': {
+                  color: this.theme.colorMod(.05, params.colors.link.text),
+                  textDecoration: 'underline',
+                },
+              }
             },
             $default: 'default',
           }],
@@ -96,6 +128,11 @@ export class KitDefaultButtonStyle implements KitComponentStyle {
             background: params.colors.border.color,
             borderColor: params.colors.border.color,
             color: params.colors.border.text,
+            $nest: {
+              '&:hover': {
+                background: params.colors.border.color,
+              },
+            }
           },
         },
       },
