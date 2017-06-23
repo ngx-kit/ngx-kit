d3651fd983b209be2dc0c1d3c0399868740c0b6b
icydew <icydew@live.com>
2017-06-23 16:07:32 +0300

    fix(default-theme): change accordion-panel style

--- packages/default-theme/components/kit-default-accordion-panel.style.ts
+++ packages/default-theme/components/kit-default-accordion-panel.style.ts
@@ -13,19 +13,36 @@ export class KitDefaultAccordionPanelStyle implements KitComponentStyle {
   }
 
   host(): StyleDef {
-    return {};
+    return {
+      backgroundColor: '#f7f7f7',
+      border: [1, 'solid', '#d9d9d9'],
+      borderBottomWidth: 0,
+      display: 'block',
+      $nest: {
+        '&:last-child': {
+          borderBottomWidth: 1,
+        },
+      },
+
+    };
   }
 
-  title(state: {active: boolean}): StyleDef {
+  title(state: { active: boolean }): StyleDef {
     return {
-      ...this.def.toggle(state.active, {
-        fontWeight: 600,
-      }),
+      cursor: 'pointer',
+      height: this.theme.params.grid.v * 4,
+      lineHeight: this.theme.params.grid.h * 4,
+      paddingLeft: this.theme.params.grid.v * 3,
+      ...this.def.toggle(state.active, {}),
     };
   }
 
   content(): StyleDef {
-    return {};
+    return {
+      padding: [this.theme.params.grid.v * 2, this.theme.params.grid.h * 3],
+      backgroundColor: '#ffffff',
+
+    };
   }
 
 }
