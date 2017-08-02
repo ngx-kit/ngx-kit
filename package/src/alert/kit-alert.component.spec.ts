4c615eee8efd6676183eb86eecda0a3afc98baae
navix <inavix@gmail.com>
2017-08-02 11:02:51 +0300

    test(alert): kit-alert&kit-alert-title spec

--- package/src/alert/kit-alert.component.spec.ts
+++ package/src/alert/kit-alert.component.spec.ts
@@ -1,22 +1,41 @@
-import { Component, DebugElement, Directive, Injectable, Input, ViewChild } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, DebugElement, Directive, Injectable, Input } from '@angular/core';
+import { async, ComponentFixture, inject, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { StylerComponent } from '@ngx-kit/styler';
-import { kitComponentSpinner } from '../core/meta/tokens';
+import { Subject } from 'rxjs/Subject';
+import { kitComponentAlert, kitComponentAlertTitle } from '../core/meta/tokens';
+import { KitAlertTitleComponent } from './kit-alert-title.component';
 import { KitAlertComponent } from './kit-alert.component';
 
-describe('Spinner/SpinnerComponent', () => {
+describe('Alert/AlertComponent', () => {
   let fixture: ComponentFixture<ContainerComponent>;
   let container: ContainerComponent;
   let element: Element;
   let de: DebugElement;
+  let styler: StylerMock;
+  let stylerForTitle: StylerMock;
+  let alert: KitAlertComponent;
+  let alertWithTitle: KitAlertComponent;
   // setup
   beforeEach(async(() =>
       TestBed.configureTestingModule({
-        declarations: [StylerDirective, ContainerComponent, KitAlertComponent],
+        declarations: [StylerDirective, ContainerComponent, KitAlertComponent, KitAlertTitleComponent],
         providers: [
           {
-            provide: kitComponentSpinner,
+            provide: kitComponentAlert,
             useClass: StyleMock,
+          },
+          {
+            provide: kitComponentAlertTitle,
+            useClass: StyleMock,
+          },
+          {
+            provide: 'styler',
+            useClass: StylerMock,
+          },
+          {
+            provide: 'stylerForTitle',
+            useClass: StylerMock,
           },
         ],
       }).overrideComponent(KitAlertComponent, {
@@ -24,7 +43,16 @@ describe('Spinner/SpinnerComponent', () => {
           viewProviders: [
             {
               provide: StylerComponent,
-              useClass: StylerMock,
+              useExisting: 'styler',
+            },
+          ],
+        },
+      }).overrideComponent(KitAlertTitleComponent, {
+        set: {
+          viewProviders: [
+            {
+              provide: StylerComponent,
+              useExisting: 'stylerForTitle',
             },
           ],
         },
@@ -35,28 +63,112 @@ describe('Spinner/SpinnerComponent', () => {
     container = fixture.componentInstance;
     element = fixture.nativeElement;
     de = fixture.debugElement;
+    alert = de.children[0].componentInstance;
+    alertWithTitle = de.children[1].componentInstance;
     fixture.detectChanges();
   });
+  beforeEach(inject(['styler', 'stylerForTitle'],
+      (stylerMock: StylerMock, stylerForTitleMock: StylerMock) => {
+        styler = stylerMock;
+        stylerForTitle = stylerForTitleMock;
+      }));
   // specs
   it('container and component instance should be created', () => {
     expect(container).toBeTruthy();
-    expect(container.instance instanceof KitAlertComponent).toBeTruthy();
+    expect(alert instanceof KitAlertComponent).toBeTruthy();
+  });
+  it('should display message', () => {
+    const message = 'text';
+    container.message = message;
+    fixture.detectChanges();
+    const alert = de.query(By.css('.alert'));
+    expect(alert.nativeElement.textContent.trim()).toEqual(message);
+  });
+  it('should pass color state', () => {
+    const color = 'color';
+    container.color = color;
+    fixture.detectChanges();
+    expect(styler.host.state.color).toEqual(color);
+  });
+  it('should display closing link', () => {
+    container.closable = true;
+    fixture.detectChanges();
+    const closingLink = de.query(By.css('button'));
+    expect(closingLink).toBeTruthy();
+  });
+  it('show change closing link text', () => {
+    const closeText = 'text';
+    container.closable = true;
+    container.closeText = closeText;
+    fixture.detectChanges();
+    const closingLink = de.query(By.css('button'));
+    expect(closingLink.nativeElement.textContent.trim()).toEqual(closeText);
+  });
+  it('should set closed state on close', () => {
+    container.closable = true;
+    fixture.detectChanges();
+    const closingLink = de.query(By.css('button'));
+    closingLink.nativeElement.click();
+    expect(styler.host.state.closed).toBeTruthy();
+  });
+  it('should emit close event on close', (done: DoneFn) => {
+    container.closeEvent.subscribe(e => {
+      expect(e).toBeNull();
+      done();
+    });
+    container.closable = true;
+    fixture.detectChanges();
+    const closingLink = de.query(By.css('button'));
+    closingLink.nativeElement.click();
+  });
+  it('should proxy color state to title', () => {
+    const color = 'color';
+    container.color = color;
+    fixture.detectChanges();
+    expect(stylerForTitle.host.state.color).toEqual(color);
   });
 });
 
-// Component with kit-spinner
+// Component with kit-alert
 @Component({
   selector: 'container',
   template: `
-    <kit-spinner></kit-spinner>
+    <kit-alert [color]="color"
+               [closable]="closable"
+               [closeText]="closeText"
+               (close)="closeEvent.next($event)"
+               class="alert">
+      {{ message }}
+    </kit-alert>
+    <kit-alert [color]="color">
+      <kit-alert-title></kit-alert-title>
+    </kit-alert>
   `,
 })
 class ContainerComponent {
-  @ViewChild(KitAlertComponent) instance: KitAlertComponent;
+  closable = false;
+
+  closeEvent = new Subject<null>();
+
+  closeText = '';
+
+  color = '';
+
+  message = '';
 }
 
 @Injectable()
 class StylerMock {
+  host = {
+    state: {
+      color: '',
+      closed: false,
+    },
+    applyState: (state: any) => {
+      this.host.state = {...this.host.state, ...state};
+    },
+  };
+
   register(style: any) {
   }
 }
