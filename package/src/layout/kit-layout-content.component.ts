48e75d0b42ef4f8896a96342d292717f8e18f1ac
navix <inavix@gmail.com>
2017-08-21 16:14:37 +0300

    feat(layout): add layout-content scrollTopOnRouting param + README

--- package/src/layout/kit-layout-content.component.ts
+++ package/src/layout/kit-layout-content.component.ts
@@ -1,4 +1,5 @@
-import { Component, Inject, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Inject, Input, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { StylerComponent } from '@ngx-kit/styler';
 import { KitComponentStyle } from '../core/meta/component';
 import { kitComponentLayoutContent } from '../core/meta/tokens';
@@ -15,12 +16,23 @@ import { kitComponentLayoutContent } from '../core/meta/tokens';
 export class KitLayoutContentComponent implements OnInit {
   @Input() kitLayoutContent: any;
 
+  @Input() scrollTopOnRouting = false;
+
   constructor(private styler: StylerComponent,
-              @Inject(kitComponentLayoutContent) private style: KitComponentStyle) {
+              @Inject(kitComponentLayoutContent) private style: KitComponentStyle,
+              private router: Router,
+              private el: ElementRef) {
     this.styler.classPrefix = 'kit-layout-content';
     this.styler.register(this.style);
   }
 
   ngOnInit() {
+    if (this.scrollTopOnRouting) {
+      this.router.events.subscribe(event => {
+        if (event instanceof NavigationEnd) {
+          this.el.nativeElement.scrollTop = 0;
+        }
+      });
+    }
   }
 }
