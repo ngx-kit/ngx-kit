74bc3f8ae7382169e676feb9960d7bdedeba9b88
icydew <icydew@live.com>
2017-08-27 11:56:33 +0300

    change website style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MdRenderModule } from '@nvxme/ngx-md-render';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { RootComponent } from './root/root.component';
+import { BemClassGenStrategy, ClassGenStategy } from '@ngx-kit/styler';
 
 @NgModule({
   imports: [
@@ -22,7 +23,12 @@ import { RootComponent } from './root/root.component';
   declarations: [
     RootComponent,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: ClassGenStategy,
+      useClass: BemClassGenStrategy,
+    }
+  ],
   bootstrap: [RootComponent],
 })
 export class AppModule {
