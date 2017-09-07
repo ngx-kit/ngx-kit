f7a89db51f3e18782b0a631c47d4a9114b4ba9ed
navix <inavix@gmail.com>
2017-09-07 19:17:31 +0300

    feat(util): make utils public

---
+++ package/src/util/index.ts
@@ -0,0 +1,7 @@
+export * from './is-defined';
+export * from './is-mergable';
+export * from './is-object';
+export * from './is-string';
+export * from './merge-deep';
+export * from './partial';
+export * from './uuid';
