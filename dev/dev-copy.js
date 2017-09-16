57d3ee1e879bdf5ec46cacc41aa651595fc2332d
navix <inavix@gmail.com>
2017-09-16 14:38:03 +0300

    build: update dev for copy sources

--- dev/dev-copy.js
+++ dev/dev-copy.js
@@ -3,7 +3,14 @@ const fs = require('fs-extra');
 
 const config = require('./dev-copy-config.json');
 
-config.paths.forEach(p => {
-  fs.copySync(path.resolve('dist/release'), path.resolve(p));
-  console.log('Dist copied', p);
+config.release.forEach(p => {
+  fs.copySync(path.resolve('./dist/release'), path.resolve(p));
+  console.log('release copied', p);
+});
+
+config.src.forEach(s => {
+  fs.copySync(path.resolve('./demo'), path.resolve(s, 'demo'));
+  fs.copySync(path.resolve('./docs'), path.resolve(s, 'docs'));
+  fs.copySync(path.resolve('./package'), path.resolve(s, 'package'));
+  console.log('src copied', s);
 });
