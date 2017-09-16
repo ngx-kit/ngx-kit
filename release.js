0f8d619275053ba4bd661ac7e3d6b10c51991263
navix <inavix@gmail.com>
2017-09-16 14:41:22 +0300

    build: move version to package.json + release 1.0.0-beta.4

--- release.js
+++ release.js
@@ -1,16 +1,15 @@
 const path = require('path');
 const fs = require('fs-extra');
 
+const pkg = require('./package.json');
 const config = require('./release.config.json');
-
-
 
 // Copy sources
 //fs.copySync(path.resolve('package'), path.resolve('dist/package'));
 // Generate package.json
 const blueprint = fs.readFileSync(path.resolve('package/package.json'), 'utf-8');
 const result = blueprint
-    .replace(/0\.0\.0\-PLACEHOLDER/g, config.version)
+    .replace(/0\.0\.0\-PLACEHOLDER/g, pkg.version)
     .replace(/0\.0\.0\-ANGULAR\-PLACEHOLDER/g, config.vendors.angular)
     .replace(/0\.0\.0\-TYPESCRIPT\-PLACEHOLDER/g, config.vendors.typescript)
     .replace(/0\.0\.0\-STYLER\-PLACEHOLDER/g, config.vendors.styler)
