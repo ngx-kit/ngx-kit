e5861d06a26f64217d3c6209bf9307eb009101d7
navix <inavix@gmail.com>
2017-09-23 16:41:36 +0300

    feat(overlay): rework KitOverlayPosition service to component-providing helper

--- package/src/overlay/kit-overlay-position.service.ts
+++ package/src/overlay/kit-overlay-position.service.ts
@@ -1,23 +1,37 @@
-import { Injectable, NgZone, OnDestroy, Renderer2 } from '@angular/core';
+import { ElementRef, Injectable, NgZone, OnDestroy, Renderer2 } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
+import { KitStyleService } from '../common/kit-style/kit-style.service';
 import { KitAnchorDirective } from './kit-anchor.directive';
-import { KitCoreOverlayContainerPosition } from './meta';
+import { KitCoreOverlayContainerPosition, KitCoreOverlayContainerType } from './meta';
 
 @Injectable()
 export class KitOverlayPositionService implements OnDestroy {
+  anchor: KitAnchorDirective | HTMLElement;
+
+  position: KitCoreOverlayContainerPosition = 'top';
+
+  type: KitCoreOverlayContainerType = 'side';
+
+  widthType: 'full' | 'auto' = 'auto';
+
   private _repositionEvent$ = new Subject<void>();
 
   private listeners: any[];
 
   constructor(private renderer: Renderer2,
-              private zone: NgZone) {
+              private zone: NgZone,
+              private el: ElementRef,
+              private style: KitStyleService) {
     this.zone.runOutsideAngular(() => {
       this.listeners = [
         this.renderer.listen('document', 'scroll', () => this._repositionEvent$.next()),
         this.renderer.listen('window', 'resize', () => this._repositionEvent$.next()),
       ];
     });
+    this._repositionEvent$.subscribe(() => {
+      this.reposition();
+    })
   }
 
   get repositionEvent$(): Observable<void> {
@@ -28,48 +42,104 @@ export class KitOverlayPositionService implements OnDestroy {
     this.listeners.forEach(l => l());
   }
 
-  repositionDropdown(anchor: KitAnchorDirective | HTMLElement, position: KitCoreOverlayContainerPosition, widthType: 'full' | 'auto') {
-    const el = this.getEl(anchor);
-    const rect: ClientRect = el.getBoundingClientRect();
-    const windowHeight = window.innerHeight;
-    switch (position) {
-      case 'top':
-        return {
-          position: 'fixed',
-          top: `${Math.round(rect.top)}px`,
-          left: `${Math.round(rect.left)}px`,
-          transform: 'translateY(-100%)',
-          width: widthType === 'full' ? `${Math.round(el.offsetWidth)}px` : null,
-          maxHeight: `${rect.top - 16}px`,
-          overflowY: 'auto',
-        };
-      case 'bottom':
-        return {
-          position: 'fixed',
-          top: `${Math.round(rect.top + el.offsetHeight)}px`,
-          left: `${Math.round(rect.left)}px`,
-          width: widthType === 'full' ? `${Math.round(el.offsetWidth)}px` : null,
-          maxHeight: `${windowHeight - rect.bottom - 16}px`,
-          overflowY: 'auto',
-        };
-      case 'left':
-        return {
-          position: 'fixed',
-          top: `${Math.round(rect.top)}px`,
-          left: `${Math.round(rect.left)}px`,
-        };
-      case 'right':
-        return {
-          position: 'fixed',
-          top: `${Math.round(rect.top)}px`,
-          left: `${Math.round(rect.right)}px`,
-        };
-      default:
-        throw new Error('In development!');
+  reposition() {
+    switch (this.type) {
+      case 'dropdown':
+        this.repositionDropdown();
+        break;
+      case 'side':
+        this.repositionSide();
+        break;
     }
   }
 
   private getEl(anchor: KitAnchorDirective | HTMLElement): HTMLElement {
     return anchor instanceof KitAnchorDirective ? anchor.nativeEl : anchor;
+  }
+
+  private repositionDropdown() {
+    const el = this.getEl(this.anchor);
+    const rect: ClientRect = el.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+    switch (this.position) {
+      case 'top':
+        this.style.style = {
+          position: 'fixed',
+          top: `${Math.round(rect.top)}px`,
+          left: `${Math.round(rect.left)}px`,
+          transform: 'translateY(-100%)',
+          width: this.widthType === 'full' ? `${Math.round(el.offsetWidth)}px` : 'auto',
+          maxHeight: `${rect.top - 16}px`,
+          overflowY: 'auto',
+        };
+        break;
+      case 'bottom':
+        this.style.style = {
+          position: 'fixed',
+          top: `${Math.round(rect.top + el.offsetHeight)}px`,
+          left: `${Math.round(rect.left)}px`,
+          width: this.widthType === 'full' ? `${Math.round(el.offsetWidth)}px` : 'auto',
+          maxHeight: `${windowHeight - rect.bottom - 16}px`,
+          overflowY: 'auto',
+        };
+        break;
+      case 'left':
+        this.style.style = {
+          position: 'fixed',
+          top: `${Math.round(rect.top)}px`,
+          left: `${Math.round(rect.left)}px`,
+        };
+        break;
+      case 'right':
+        this.style.style = {
+          position: 'fixed',
+          top: `${Math.round(rect.top)}px`,
+          left: `${Math.round(rect.right)}px`,
+        };
+        break;
+      default:
+        throw new Error('In development!');
+    }
+  }
+
+  private repositionSide() {
+    const el = this.getEl(this.anchor);
+    const rect: ClientRect = el.getBoundingClientRect();
+    switch (this.position) {
+      case 'top':
+        this.style.style = {
+          position: 'fixed',
+          top: `${Math.round(rect.top)}px`,
+          left: `${Math.round(rect.left + el.offsetWidth / 2)}px`,
+          transform: 'translateX(-50%) translateY(-100%)',
+        };
+        break;
+      case 'bottom':
+        this.style.style = {
+          position: 'fixed',
+          top: `${Math.round(rect.bottom)}px`,
+          left: `${Math.round(rect.left + el.offsetWidth / 2)}px`,
+          transform: 'translateX(-50%)',
+        };
+        break;
+      case 'left':
+        this.style.style = {
+          position: 'fixed',
+          top: `${Math.round(rect.top + el.offsetHeight / 2)}px`,
+          left: `${Math.round(rect.left)}px`,
+          transform: 'translateX(-100%) translateY(-50%)',
+        };
+        break;
+      case 'right':
+        this.style.style = {
+          position: 'fixed',
+          top: `${Math.round(rect.top + el.offsetHeight / 2)}px`,
+          left: `${Math.round(rect.right)}px`,
+          transform: 'translateY(-50%)',
+        };
+        break;
+      default:
+        throw new Error('In development!');
+    }
   }
 }
