699f04d54695c39848eb60d4a768713aebe7fd81
navix <inavix@gmail.com>
2017-10-02 20:04:49 +0300

    feat(common): add *kitRepeat directive

--- package/src/common/kit-common.module.ts
+++ package/src/common/kit-common.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { StylerModule } from '@ngx-kit/styler';
 import { KitClassDirective } from './kit-class/kit-class.directive';
 import { KitMultiOutletDirective } from './kit-multi-outlet/kit-multi-outlet.directive';
+import { KitRepeatDirective } from './kit-repeat/kit-repeat.directive';
 import { KitTreeClickDirective } from './kit-tree-click.directive';
 
 const exports = [
   KitMultiOutletDirective,
+  KitRepeatDirective,
   KitTreeClickDirective,
   KitClassDirective,
 ];
