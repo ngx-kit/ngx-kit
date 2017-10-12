d43b66c6ac844e4f088bbbb76c2487d52c23f19c
navix <inavix@gmail.com>
2017-10-12 11:26:03 +0300

    fix: restore demo working on prod build

--- src/app/shared/demo/demo.component.ts
+++ src/app/shared/demo/demo.component.ts
@@ -1,8 +1,8 @@
-import { Component, ElementRef, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { StylerComponent, StylerModule } from '@ngx-kit/styler';
 import { MdRenderService } from '@nvxme/ngx-md-render';
 import { highlightAuto } from 'highlight.js';
-import { demo } from '../../../packages/ui-base/package/demo/demo';
+import { demo } from '../../../packages/ui-base/package/demo/demo-ref';
 import { DemoStyle } from './demo.style';
 
 @Component({
@@ -26,7 +26,6 @@ export class DemoComponent implements OnChanges {
   readme: string;
 
   constructor(private styler: StylerComponent,
-              private el: ElementRef,
               private md: MdRenderService) {
   }
 
@@ -34,7 +33,13 @@ export class DemoComponent implements OnChanges {
     if (!this.demo) {
       throw new Error('Demo is not passed!');
     }
-    this.class = demo.find(d => Reflect.get(d, 'name') === this.demo.class);
+    if (!demo) {
+      throw new Error('demo-ref is not generated!');
+    }
+    if (!demo[this.demo.class]) {
+      throw new Error(`Class ${this.demo.class} not found in demo-ref`);
+    }
+    this.class = demo[this.demo.class];
     console.log('class', this.class);
     this.readme = this.md.render(this.demo.readme);
     this.code = this.demo.code.reduce((prev, file) => ({
