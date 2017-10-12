02266ce737d4c590cce9a8ffd46a81073888d400
navix <inavix@gmail.com>
2017-10-12 07:34:10 +0300

    fix: turnoff home-page animations (bug with double run)

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,3 @@
-import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 import { StylerComponent, StylerModule } from '@ngx-kit/styler';
 import { LayoutStyle } from '../shared/layout/layout.style';
@@ -12,18 +11,19 @@ import { HomePageStyle } from './home-page.style';
     StylerModule.forComponent(HomePageStyle),
   ],
   animations: [
-    trigger('products', [
-      transition(':enter', [
-        query('a', style({
-          opacity: 0,
-          transform: 'translateY(-20%)',
-        }), {optional: true}),
-        query('a', stagger(150, animate('400ms ease-in', style({
-          opacity: 1,
-          transform: 'translateY(0)',
-        }))), {optional: true}),
-      ]),
-    ]),
+    // @todo do not run animations second time (first time from universal, second after ng mount)
+//    trigger('products', [
+//      transition(':enter', [
+//        query('a', style({
+//          opacity: 0,
+//          transform: 'translateY(-20%)',
+//        }), {optional: true}),
+//        query('a', stagger(150, animate('400ms ease-in', style({
+//          opacity: 1,
+//          transform: 'translateY(0)',
+//        }))), {optional: true}),
+//      ]),
+//    ]),
   ],
 })
 export class HomePageComponent {
