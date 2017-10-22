0e310d5957cd7a7d25820d55bf6934b255ce7770
navix <inavix@gmail.com>
2017-10-22 17:22:50 +0300

    fix(focus-manager): remove KitGridControlDirective declaration from grid module

--- packages/ngx-kit/src/kit-focus-manager/kit-focus-manager.module.ts
+++ packages/ngx-kit/src/kit-focus-manager/kit-focus-manager.module.ts
@@ -1,18 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { KitFocusDirective } from './kit-focus/kit-focus.directive';
-import { KitGridControlDirective } from '../kit-grid/kit-grid-control/kit-grid-control.directive';
 
 @NgModule({
   imports: [
     CommonModule,
   ],
   declarations: [
-    KitGridControlDirective,
     KitFocusDirective,
   ],
   exports: [
-    KitGridControlDirective,
     KitFocusDirective,
   ],
 })
