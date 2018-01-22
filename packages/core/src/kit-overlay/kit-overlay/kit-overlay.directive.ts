17df1d876b330bc045d8eeef5b539ed636cdc847
Sasha Novyk <inavix@gmail.com>
2018-01-22 11:48:58 +0200

    feat(core/modal): nested modals support, move backdrop to the last modal

--- packages/core/src/kit-overlay/kit-overlay/kit-overlay.directive.ts
+++ packages/core/src/kit-overlay/kit-overlay/kit-overlay.directive.ts
@@ -1,13 +1,9 @@
 import {
-  ChangeDetectorRef,
-  Directive,
-  Input,
-  OnChanges,
-  OnDestroy,
-  SimpleChanges,
-  TemplateRef,
+  ChangeDetectorRef, Directive, Input, OnChanges, OnDestroy, SimpleChanges, TemplateRef,
   ViewRef,
 } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 import { KitOverlayService } from '../kit-overlay.service';
 
@@ -19,13 +15,23 @@ export class KitOverlayDirective implements OnChanges, OnDestroy {
 
   private doCheckSub: Subscription;
 
-  private viewRef: ViewRef | null;
+  private _viewRef: ViewRef | null;
+
+  private _displayed = new BehaviorSubject<boolean>(false);
 
   constructor(
     private templateRef: TemplateRef<any>,
     private service: KitOverlayService,
     private cdr: ChangeDetectorRef,
   ) {
+  }
+
+  get displayed(): Observable<boolean> {
+    return this._displayed.asObservable();
+  }
+
+  get viewRef(): ViewRef | null {
+    return this._viewRef;
   }
 
   ngOnChanges(changes: SimpleChanges) {
@@ -39,24 +45,28 @@ export class KitOverlayDirective implements OnChanges, OnDestroy {
   }
 
   updateHost() {
-    if (this.kitOverlay && !this.viewRef) {
-      this.viewRef = this.service.hostTemplate(this.templateRef, {});
-      this.viewRef.detectChanges();
+    if (this.kitOverlay && !this._viewRef) {
+      this._viewRef = this.service.hostTemplate(this.templateRef, {kek: '123KEK'});
+      this._viewRef.detectChanges();
       this.doCheckSub = this.service.onHostStable.subscribe(() => {
-        this.cdr.detectChanges();
+        if (!this.cdr['destroyed']) {
+          this.cdr.detectChanges();
+        }
       });
+      this._displayed.next(true);
     } else if (!this.kitOverlay) {
       this.destroyView();
       if (this.doCheckSub) {
         this.doCheckSub.unsubscribe();
       }
+      this._displayed.next(false);
     }
   }
 
   private destroyView() {
-    if (this.viewRef) {
-      this.viewRef.destroy();
-      this.viewRef = null;
+    if (this._viewRef) {
+      this._viewRef.destroy();
+      this._viewRef = null;
     }
   }
 }
