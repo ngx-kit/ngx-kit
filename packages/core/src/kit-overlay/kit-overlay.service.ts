1cce4c870c2bfb903fd73e7fba473f16c03e0865
Sasha Novyk <inavix@gmail.com>
2018-03-16 14:50:14 +0200

    * fix(core/overlay): "no component factory found" in lazy modules

--- packages/core/src/kit-overlay/kit-overlay.service.ts
+++ packages/core/src/kit-overlay/kit-overlay.service.ts
@@ -4,9 +4,12 @@ import {
   ComponentFactoryResolver,
   ComponentRef,
   EmbeddedViewRef,
+  forwardRef,
   Inject,
   Injectable,
   Injector,
+  Optional,
+  SkipSelf,
   TemplateRef,
   Type,
   ViewRef,
@@ -27,39 +30,53 @@ export class KitOverlayService {
 
   private container: Element;
 
+  private isRoot: boolean;
+
   constructor(
     @Inject(DOCUMENT) private document: any,
     private cfr: ComponentFactoryResolver,
     private appRef: ApplicationRef,
     private injector: Injector,
+    @Optional() @Inject(forwardRef(() => KitOverlayService)) @SkipSelf() private parent: KitOverlayService,
   ) {
-    this.mountHost();
+    this.isRoot = !this.parent;
+    if (this.isRoot) {
+      this.mountHost();
+    }
   }
 
   get onHostStable(): Observable<void> {
-    return this._onHostStable.asObservable();
+    return this.isRoot
+      ? this._onHostStable.asObservable()
+      : this.parent.onHostStable;
   }
 
   /**
    * Render component on the overlay.
    */
   hostComponent<T>(component: Type<T>, providers: StaticProvider[] = [], cfr?: ComponentFactoryResolver): ComponentRef<T> {
-    const id = uuid();
-    const injector = Injector.create({
-      providers,
-      parent: this.host.vcr.parentInjector,
-    });
-    const componentFactory = cfr
-      ? cfr.resolveComponentFactory(component)
-      : this.cfr.resolveComponentFactory(component);
-    return this.host.vcr.createComponent<T>(componentFactory, this.host.vcr.length, injector);
+    if (this.isRoot) {
+      const id = uuid();
+      const injector = Injector.create({
+        providers,
+        parent: this.host.vcr.parentInjector,
+      });
+      const componentFactory = cfr
+        ? cfr.resolveComponentFactory(component)
+        : this.cfr.resolveComponentFactory(component);
+      return this.host.vcr.createComponent<T>(componentFactory, this.host.vcr.length, injector);
+    } else {
+      return this.parent.hostComponent(component, providers, cfr || this.cfr);
+    }
   }
 
   /**
    * Render template (passed by TemplateRef) on the overlay.
    */
   hostTemplate(templateRef: TemplateRef<any>, context: any = {}): ViewRef {
-    return this.host.vcr.createEmbeddedView(templateRef, context);
+    return this.isRoot
+      ? this.host.vcr.createEmbeddedView(templateRef, context)
+      : this.parent.hostTemplate(templateRef);
   }
 
   /**
@@ -67,11 +84,18 @@ export class KitOverlayService {
    * Used for multi-modals backdrop handling.
    */
   moveUnder(ref: ViewRef, target: ViewRef) {
-    const targetIndex = this.host.vcr.indexOf(target);
-    this.host.vcr.move(ref, targetIndex - 1);
+    if (this.isRoot) {
+      const targetIndex = this.host.vcr.indexOf(target);
+      this.host.vcr.move(ref, targetIndex - 1);
+    } else {
+      this.parent.moveUnder(ref, target);
+    }
   }
 
   private mountHost() {
+    if (!this.isRoot) {
+      throw new Error(`Run .mountHost() only for root service`);
+    }
     // Append container
     this.container = this.document.createElement('div');
     this.container.classList.add('kit-overlay-container');
