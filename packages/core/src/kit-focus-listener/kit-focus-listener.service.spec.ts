7c2f5fd42b9fb3edf814edf5bfe5127e1732341a
Sasha Novyk <inavix@gmail.com>
2018-03-21 18:45:55 +0200

    feat(core/focus-listener): add KitFocusListenerService

---
+++ packages/core/src/kit-focus-listener/kit-focus-listener.service.spec.ts
@@ -0,0 +1,62 @@
+import { async } from '@angular/core/testing';
+import { KitFocusListenerService } from './kit-focus-listener.service';
+
+describe('KitFocusListenerService', () => {
+  let service: KitFocusListenerService;
+  let emStub: EventManagerStub;
+  beforeEach(async(() => {
+    emStub = new EventManagerStub();
+    service = new KitFocusListenerService(emStub as any);
+  }));
+  it('should create the service', () => {
+    expect(service).toBeTruthy();
+  });
+  describe('.register()', () => {
+    it('adds listener', () => {
+      const spy = spyOn(emStub, 'addEventListener');
+      service.register(true as any);
+      expect(spy).toHaveBeenCalled();
+    });
+    it('emits focus on gains focus', (done) => {
+      const elSample = document.createElement('div');
+      service.focus.subscribe(() => {
+        expect(true).toBeTruthy();
+        done();
+      });
+      service.register(elSample);
+      emStub.emitFocus();
+    });
+    it('emits blur on loosing focus', (done) => {
+      const elSample = document.createElement('div');
+      service.blur.subscribe(() => {
+        expect(true).toBeTruthy();
+        done();
+      });
+      service.register(elSample);
+      emStub.emitBlur();
+    });
+  });
+});
+
+class EventManagerStub {
+  focusHandler: Function;
+
+  blurHandler: Function;
+
+  emitFocus() {
+    this.focusHandler({});
+  }
+
+  emitBlur() {
+    this.blurHandler({});
+  }
+
+  addEventListener(el: any, type: string, handler: Function) {
+    if (type === 'focus') {
+      this.focusHandler = handler;
+    }
+    if (type === 'blur') {
+      this.blurHandler = handler;
+    }
+  }
+}
