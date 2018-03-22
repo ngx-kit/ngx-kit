dcd9befc0d1ec6678232ed292aef1995e1681b01
Sasha Novyk <inavix@gmail.com>
2018-03-22 11:26:38 +0200

    feat(core/value-accessor): add KitValueAccessor

---
+++ packages/core/src/kit-value-accessor/kit-model-interceptor.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+
+@Injectable()
+export abstract class KitModelInterceptor {
+  viewStateChanges: Subject<string>;
+
+  modelStateChanges: Subject<any>;
+
+  abstract input(value: string, event: any): void;
+
+  abstract keyDown(event: any): void;
+
+  abstract writeValue(value: any): void;
+}
