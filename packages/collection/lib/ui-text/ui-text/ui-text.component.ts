7bb49a6914272501a88efe4542773149335744d1
Sasha Novyk <inavix@gmail.com>
2018-04-02 15:24:55 +0300

    feat(collection/text): integrate UiText with KitFormField, errors styling

--- packages/collection/lib/ui-text/ui-text/ui-text.component.ts
+++ packages/collection/lib/ui-text/ui-text/ui-text.component.ts
@@ -1,4 +1,13 @@
-import { ChangeDetectionStrategy, Component, Input, } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  HostBinding,
+  Input,
+  OnInit,
+  Optional,
+} from '@angular/core';
+import { KitFormFieldService } from '@ngx-kit/core';
 
 @Component({
   // tslint:disable-next-line
@@ -7,6 +16,23 @@ import { ChangeDetectionStrategy, Component, Input, } from '@angular/core';
   styleUrls: ['./ui-text.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UiTextComponent {
+export class UiTextComponent implements OnInit {
   @Input() uiText: void;
+
+  @HostBinding('class.-has-errors') hasErrorsClass = false;
+
+  constructor(
+    @Optional() private formFieldService: KitFormFieldService,
+    private cdr: ChangeDetectorRef,
+  ) {
+  }
+
+  ngOnInit() {
+    if (this.formFieldService) {
+      this.formFieldService.control.errorStateChanges.subscribe(errors => {
+        this.hasErrorsClass = !errors || errors.length > 0;
+        this.cdr.detectChanges();
+      });
+    }
+  }
 }
