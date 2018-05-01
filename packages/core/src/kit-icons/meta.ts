f2295c4115bff076b442158af4fb9e968af9c9f0
Sasha Novyk <inavix@gmail.com>
2018-05-01 11:47:06 +0300

    feat(core/icons): introduce .add() registry method with size property
    Closes #68
    
    Registry .register() and .registerSet() are now deprecated.

--- packages/core/src/kit-icons/meta.ts
+++ packages/core/src/kit-icons/meta.ts
@@ -1,6 +1,12 @@
 export interface KitIcon {
   name: string;
   url: string;
+  size?: string;
+}
+
+export interface KitIconNode {
+  svg: SVGElement;
+  size?: string;
 }
 
 export interface KitIconCached {
