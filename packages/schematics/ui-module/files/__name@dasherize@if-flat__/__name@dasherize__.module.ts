1775dfd7ef3295f43ed2cf38ada9f74010826b20
Sasha Novyk <inavix@gmail.com>
2018-06-04 20:30:08 +0300

    feat(schematics): remove old schematics, add ui-module schematic

---
+++ packages/schematics/ui-module/files/__name@dasherize@if-flat__/__name@dasherize__.module.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { <%= classify(name) %>Component } from './<%= dasherize(name) %>/<%= dasherize(name) %>.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+  ],
+  declarations: [
+    <%= classify(name) %>Component,
+  ],
+  exports: [
+    <%= classify(name) %>Component,
+  ],
+})
+export class <%= classify(name) %>Module {
+}
