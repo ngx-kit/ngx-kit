21907c809562b2c99be42a6b381c05b2744f9a74
Sasha Novyk <inavix@gmail.com>
2018-06-12 14:25:33 +0300

    fix(schematics): ui-module, import module to demo

--- packages/schematics/ui-module/files/__name@dasherize@if-flat__/demo/__name@dasherize__-demo.module.ts
+++ packages/schematics/ui-module/files/__name@dasherize@if-flat__/demo/__name@dasherize__-demo.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { <%= classify(name) %>DemoComponent } from './<%= dasherize(name) %>-demo.component';
+import { <%= classify(name) %>Module } from '../<%= dasherize(name) %>.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    <%= classify(name) %>Module,
   ],
   declarations: [
     <%= classify(name) %>DemoComponent,
