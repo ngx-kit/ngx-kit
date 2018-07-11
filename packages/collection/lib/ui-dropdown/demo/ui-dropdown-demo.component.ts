84f3d03dfccfea010b96d8cf833fc08c507c135a
Sasha Novyk <inavix@gmail.com>
2018-07-11 13:57:49 +0300

    style(collection): fix stylelint errors

--- packages/collection/lib/ui-dropdown/demo/ui-dropdown-demo.component.ts
+++ packages/collection/lib/ui-dropdown/demo/ui-dropdown-demo.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './ui-dropdown-demo.component.html',
-  styleUrls: ['./ui-dropdown-demo.component.scss'],
 })
 export class UiDropdownDemoComponent {
 }
