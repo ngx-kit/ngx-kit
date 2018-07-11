31e741a96f33e698efb31c1b07ac8937986ec79b
Sasha Novyk <inavix@gmail.com>
2018-07-11 14:56:58 +0300

    feat(core/overlay): remove kit-overlay-position directive as service
    BREAKING CHANGE: kit-overlay-position was removed as DEPRECATED

--- packages/core/src/kit-overlay/kit-overlay.module.ts
+++ packages/core/src/kit-overlay/kit-overlay.module.ts
@@ -2,9 +2,8 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { KitOverlayHostWrapperComponent } from './kit-overlay-host/kit-overlay-host-wrapper.component';
 import { KitOverlayHostComponent } from './kit-overlay-host/kit-overlay-host.component';
-import { KitOverlayPositionDirective } from './kit-overlay-position/kit-overlay-position.directive';
-import { KitOverlayDirective } from './kit-overlay/kit-overlay.directive';
 import { KitOverlayToggleDirective } from './kit-overlay-toggle/kit-overlay-toggle.directive';
+import { KitOverlayDirective } from './kit-overlay/kit-overlay.directive';
 
 @NgModule({
   imports: [
@@ -12,14 +11,12 @@ import { KitOverlayToggleDirective } from './kit-overlay-toggle/kit-overlay-togg
   ],
   exports: [
     KitOverlayDirective,
-    KitOverlayPositionDirective,
     KitOverlayToggleDirective,
   ],
   declarations: [
     KitOverlayDirective,
     KitOverlayHostWrapperComponent,
     KitOverlayHostComponent,
-    KitOverlayPositionDirective,
     KitOverlayToggleDirective,
   ],
   entryComponents: [
