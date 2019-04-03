aa7af8659b365ee0fbb8e31e8f25a335cdfe853c
Oleksa Novyk <oleksanovyk@gmail.com>
2019-04-03 20:51:04 +0300

    feat(ui/text): theming, update styles, autoResize

--- packages/ui/ui-text/README.md
+++ packages/ui/ui-text/README.md
@@ -1,1 +1,57 @@
 # Text component for Angular
+
+## Installation
+
+```
+$ ng add @ngx-kit/ui-text
+```
+
+
+## Usage
+
+```html
+<input type="text" uiText>
+<textarea uiText [autoResize]="true"></textarea>
+```
+
+
+## Options
+
+### `[uiText]`
+
+none
+
+### `textarea[uiText]`
+
+* `[autoResize]`
+
+
+## Theming
+
+```
+--ui-input-background
+--ui-input-border-width
+--ui-input-border-color
+--ui-input-border-radius
+--ui-input-box-shadow
+--ui-input-color
+--ui-input-font-family
+--ui-input-font-size
+--ui-input-padding
+--ui-input-hover-color
+--ui-input-hover-border-color
+--ui-input-hover-background
+--ui-input-hover-box-shadow
+--ui-input-focus-color
+--ui-input-focus-border-color
+--ui-input-focus-background
+--ui-input-focus-box-shadow
+--ui-input-focus-outline
+--ui-input-focus-outline-offset
+--ui-input-error-color
+--ui-input-error-border-color
+--ui-input-error-background
+--ui-input-error-box-shadow
+--ui-input-error-outline
+--ui-input-error-outline-offset
+```
--- packages/ui/ui-text/src/ui-text/ui-text.component.ts
+++ packages/ui/ui-text/src/ui-text/ui-text.component.ts
@@ -2,12 +2,15 @@ import {
   ChangeDetectionStrategy,
   ChangeDetectorRef,
   Component,
+  ElementRef,
   HostBinding,
+  HostListener,
   Input,
   OnInit,
   Optional,
 } from '@angular/core';
-import { KitFormFieldService } from '@ngx-kit/core';
+import { NgControl } from '@angular/forms';
+import { KitFormFieldService, KitPlatformService } from '@ngx-kit/core';
 
 @Component({
   // tslint:disable-next-line
@@ -19,20 +22,54 @@ import { KitFormFieldService } from '@ngx-kit/core';
 export class UiTextComponent implements OnInit {
   @Input() uiText: void;
 
+  @Input() autoResize = false;
+
   @HostBinding('class.-has-errors') hasErrorsClass = false;
 
   constructor(
     @Optional() private formFieldService: KitFormFieldService,
+    @Optional() private ngControl: NgControl,
     private cdr: ChangeDetectorRef,
+    private el: ElementRef,
+    private platform: KitPlatformService,
   ) {
   }
 
   ngOnInit() {
+    // Control error state
     if (this.formFieldService) {
-      this.formFieldService.control.errorStateChanges.subscribe(errors => {
-        this.hasErrorsClass = !errors || errors.length > 0;
-        this.cdr.detectChanges();
-      });
+      this.formFieldService.control
+        .errorStateChanges
+        .subscribe(errors => {
+          this.hasErrorsClass = !errors || errors.length > 0;
+          this.cdr.detectChanges();
+        });
+    }
+    // Update size on model change
+    if (this.ngControl && this.ngControl.valueChanges) {
+      this.ngControl
+        .valueChanges
+        .subscribe(() => {
+          this.resize();
+        });
+    }
+  }
+
+  @HostListener('keyup') keydownHandler() {
+    this.resize();
+  }
+
+  @HostListener('input') inputHandler() {
+    this.resize();
+  }
+
+  private resize() {
+    if (this.autoResize && this.platform.isBrowser()) {
+      const el = this.el.nativeElement;
+      // clean up hack
+      el.style.height = 'auto';
+      // set height
+      el.style.height = el.scrollHeight + el.offsetHeight - el.clientHeight + 'px';
     }
   }
 }
